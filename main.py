407cb4f6b8606dfc4b5a41c69ffc8cb2383f45c5
Varun Grover <grover82vinni@gmail.com>
2021-01-20 22:32:16 +0530

    csv

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
+# entering data in csv file
+import csv
+
+def write_csv(info_added): # w+ - write mode # name of file # for new line # a- append mode
+    with open('student_info.csv', 'a',newline='')as csv_file: # variable holding file object
+        writer = csv.writer(csv_file)
+
+        if csv_file.tell() == 0: # to check whether file is empty or not
+            writer.writerow(["First_name", "Last_name", "Class", "Age", "Mobile_no", "E-mail"])
+        
+        writer.writerow(info_added)
+
+
 # input 
 
 condition = True
@@ -10,6 +23,13 @@ while (condition):
     # print('Entered info: ' + stu_name,str(stu_age),str(stu_class)) # printing entered data
 
     print('You entered : '+ stu) # printing entered data
+
+    #split to create list
+    entered_info = stu.split(' ')
+    print(entered_info)
+
+    # function call - add info
+    write_csv(entered_info)
 
     addstu = input('Do you want to add new student info ? Type - yes/no: ')
     if addstu == 'yes' or addstu == 'YES':
