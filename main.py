e841a4e50d92e08f044fc58bcdb45ed9f1da5a7f
Varun Grover <grover82vinni@gmail.com>
2021-01-21 21:37:19 +0530

    final

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import csv
 
 def write_csv(info_added): # w+ - write mode # name of file # for new line # a- append mode
+    
     with open('student_info.csv', 'a',newline='')as csv_file: # variable holding file object
         writer = csv.writer(csv_file)
 
@@ -11,31 +12,57 @@ def write_csv(info_added): # w+ - write mode # name of file # for new line # a- 
         writer.writerow(info_added)
 
 
-# input 
+if __name__ == '__main__':
+    # input
+    condition = True
+    student_num = 1
 
-condition = True
+    while (condition):
+        print('Entered Student #{} info: '.format(student_num))
+        stu_firstname = input('First Name: ')
+        stu_lastname = input('Last Name: ')
+        stu_class = input('Class: ')
+        stu_age = input('Age: ')
+        stu_mob = input('Mobile No.: ')
+        stu_mail = input('E-mail: ')
 
-while (condition):
-    stu = input('Enter student information in the format First_name Last_name Class Age Mobile_no E-mail : ')
-    # stu_name = input('Name: ')
-    # stu_age = int(input('Age: '))
-    # stu_class = int(input('Class: '))
-    # print('Entered info: ' + stu_name,str(stu_age),str(stu_class)) # printing entered data
+        # entered info
+        print('\nEntered info: ')
+        print("First Name: " + stu_firstname)
+        print("Last Name: " + stu_lastname)
+        print("Class: " + stu_class)
+        print("Age: " + stu_age)
+        print("Mobile No.: " + stu_mob)
+        print("E-mail: " + stu_mail)
 
-    print('You entered : '+ stu) # printing entered data
+        # entering the  data in a single string
+        ent_info = stu_firstname +" " +  stu_lastname + " " + stu_class + " "  + stu_age + " "  + stu_mob + " "  + stu_mail
 
-    #split to create list
-    entered_info = stu.split(' ')
-    print(entered_info)
+        #split to create list
+        entered_info = ent_info.split(' ')
+        # print(entered_info)
 
-    # function call - add info
-    write_csv(entered_info)
+        choice = input('\nIs the entered info is correct ? (yes/no) : ')
 
-    addstu = input('Do you want to add new student info ? Type - yes/no: ')
-    if addstu == 'yes' or addstu == 'YES':
-        condition = True
-    elif addstu == 'no' or addstu == 'NO':
-        condition = False
-    else:
-        print('Please enter a valid info !!')
+        if choice == 'yes' or choice == 'YES':
+            # function call - add info
+            write_csv(entered_info)
+
+            addstu = input('Do you want to add new student info ? Type - yes/no: ')
+            if addstu == 'yes' or addstu == 'YES':
+                condition = True
+                student_num = student_num + 1
+
+            elif addstu == 'no' or addstu == 'NO':
+                condition = False
+            else:
+                print('Please enter a valid info !!')
+        
+        elif choice == 'no' or choice == 'NO':
+            print('Please re-enter the values!!')
+        
+        else:
+            print('Please enter the correct value!!')
+        
+        
 
